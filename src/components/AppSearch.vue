1d981de0a881cbf689fc1b74fb94df7a9c4ae375
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-05-10 09:56:21 +0200

    add: search

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,27 @@
+<script >
+import { store } from "../store";
+export default {
+ 
+  data() {
+    return {
+     store,
+    };
+  },
+
+ 
+ 
+};
+
+</script>
+
+<template>
+   <div>
+    <input type="cerca" name=""  v-model="store.searchQuery"  >
+    <button type="cerca" @click="$emit('cerca')" >cerca</button>
+   </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
